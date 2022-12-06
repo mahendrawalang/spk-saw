92959cdf98ae32bfd77c24d11e191b4a08e40563
yusril <mahendrashaw98@gmail.com>
2022-12-06 07:01:59 +0700

    crud

--- spk-saw-app/app/Views/Karyawan/index.php
+++ spk-saw-app/app/Views/Karyawan/index.php
@@ -1,50 +1,59 @@
-<?= $this->extend('layout/pages_layout')?>
-
-<?= $this->section('content')?>
+<?= $this->extend('layout/page_layout') ?>
+<?= $this->section('content'); ?>
 
 <div class="container">
-<h1>Halaman Data Karyawan</h1>
-<a href="<?=base_url('karyawan/add')?>" class="btn btn-warning">tambah data baru</a>
-<br><br>
-<div class="row">
-    <div class="col">
-    <table id="example" class="table table-striped" style="width:100%">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Age</th>
-                <th>Start date</th>
-                <th>Salary</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Tiger Nixon</td>
-                <td>System Architect</td>
-                <td>Edinburgh</td>
-                <td>61</td>
-                <td>2011-04-25</td>
-                <td>$320,800</td>
-            </tr>
-            <tr>
-                <td>Garrett Winters</td>
-                <td>Accountant</td>
-                <td>Tokyo</td>
-                <td>63</td>
-                <td>2011-07-25</td>
-                <td>$170,750</td>
-            </tr>
-</tbody>
-</table>
+    <h1>Karyawan</h1>
+    <a class="btn btn-primary" href="<?= base_url("/karyawan/add") ?>" role="button"> Tambah Data Karyawan Baru</a>
+    <br><br>
+    <?php if (session()->getFlashdata('pesan')) : ?>
+        <div class="alert alert-success" role="alert">
+            <?= session()->getFlashData('pesan'); ?>
+        </div>
+    <?php endif; ?>
+    <div class="row">
+        <div class="col">
+            <table id="karyawan" class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>NIP</th>
+                        <th>Nama</th>
+                        <th>Jenis Kelamin</th>
+                        <th>Telepon</th>
+                        <th>Alamat</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $no = 0;
+                    foreach ($karyawan as $key) {
+                        $no++;
+                    ?>
+                        <tr>
+                            <td><?= $no; ?></td>
+                            <td><?= $key['nip']; ?></td>
+                            <td><?= $key['nama']; ?></td>
+                            <td><?= $key['jns_kel']; ?></td>
+                            <td><?= $key['no_hp']; ?></td>
+                            <td><?= $key['alamat']; ?></td>
+                            <td><a href="/karyawan/ubah/<?= $key['id_karyawan']; ?>" class="btn btn-warning">edit</a>
+                                <form action="/karyawan/<?= $key['id_karyawan']; ?>" method="post" class="d-inline">
+                                    <?= csrf_field(); ?>
+                                    <input type="hidden" name="_method" value="DELETE">
+                                    <button type="submit" class="btn btn-danger" onclick="return confirm('Yakin Mau Menghapus Data Ini !!!')">hapus</button>
+                                </form>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
-</div>
-</div>
-
 <script>
-$(document).ready(function () {
-    $('#example').DataTable();
-});
-</script>    
-<?= $this->endSection()?>
+    $(document).ready(function() {
+        $('#karyawan').DataTable();
+    });
+</script>
+<?= $this->endSection(); ?>
--- spk-saw-app/app/Views/KriteriaPeriode/index.php
+++ spk-saw-app/app/Views/KriteriaPeriode/index.php
@@ -1,4 +1,4 @@
-<?= $this->extend('layout/pages_layout')?>
+<?= $this->extend('layout/page_layout')?>
 
 <?= $this->section('content')?>
 
